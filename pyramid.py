ed0567e79b40a759fa3cefcd20fe948adb53f27c
tarnveerSingh <66130223+tarnveerSingh@users.noreply.github.com>
2021-08-27 20:33:56 +1000

    pyra.py

---
+++ pyramid.py
@@ -0,0 +1,25 @@
+n= int(input("enter the number of rows:"))
+
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print("*", end="")
+    print()
+print("")
+for i in range(n,0,-1):
+    for j in range(0,i):
+        print("*",end="")
+    print()
+    
+print("")
+for i in range(n,0,-1):
+    for j in range(0,i):
+        print("*",end="")
+    print()
+    
+print("")
+
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print("*", end="")
+    print()
+        
